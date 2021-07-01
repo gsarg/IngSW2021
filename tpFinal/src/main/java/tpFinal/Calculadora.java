f0602a24fa5fd30e425f41c60e02a04371d21ddd
Gaston <gastonverdoia@gmail.com>
2021-06-30 22:12:37 -0300

    test sonar

--- tpFinal/src/main/java/tpFinal/Calculadora.java
+++ tpFinal/src/main/java/tpFinal/Calculadora.java
@@ -5,4 +5,12 @@ public class Calculadora {
 	public Integer sumar(Integer a, Integer b) {
 		return a + b;
 	}
+	
+//	public Integer sumar(Integer a, Integer b) {
+//		return a + b;
+//	}
+	
+	private void test() {
+		System.out.println("test");
+	}
 }
